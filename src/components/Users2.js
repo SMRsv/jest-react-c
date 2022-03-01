19638de8c7414710621f9da7b9ff908b79f982cf
SMRsv <semramirez127@gmail.com>
2022-02-28 19:24:59 -0600

    Added snapshot test

--- src/components/Users2.js
+++ src/components/Users2.js
@@ -5,7 +5,7 @@ const Users2 = (props) => {
   const legal = user.age >= 18 ? (
     <h2 style={{color: "green"}}>{user.name}</h2>
   ) : (
-    <h2 style={{color: "red"}}>{user.name}</h2> 
+    <h2 style={{color: "orange"}}>{user.name}</h2> 
   );
   return (
     <div data-testid={`user-${user.id}`}>{legal}</div>
--- src/components/__tests__/Users2.test.js
+++ src/components/__tests__/Users2.test.js
@@ -1,4 +1,5 @@
 import {render, screen, cleanup} from "@testing-library/react";
+import renderer from "react-test-renderer";
 import Users2 from "../Users2";
 
 afterEach(()=>{
@@ -11,7 +12,7 @@ test("Should render illegal user", () => {
     const userElement = screen.getByTestId("user-1");
     expect(userElement).toBeInTheDocument();
     expect(userElement).toHaveTextContent("John");
-    expect(userElement).toContainHTML("red");
+    expect(userElement).toContainHTML("orange");
 });
 
 test("Should render legal user", () => {
@@ -21,4 +22,10 @@ test("Should render legal user", () => {
     expect(userElement).toBeInTheDocument();
     expect(userElement).toHaveTextContent("Kelly");
     expect(userElement).toContainHTML("green");
+});
+
+test("Matches Snapshot", () => {
+    const user = {id: 1, name: "John", age: 15};
+    const tree = renderer.create(<Users2 user={user} />).toJSON();
+    expect(tree).toMatchSnapshot();
 });
